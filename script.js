149f2f00cf77f7e6f6e9716ffeab94425b0aadf2
Jacqueline-Martinez0408 <jacquiriosmar@gmail.com>
2025-05-28 13:02:58 -0600

    Create contenido.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Bloquear JavaScript si es necesario
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Para mayor seguridad, considera desactivar JavaScript en tu navegador.");
+});
